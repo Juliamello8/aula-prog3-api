e9902e3d5a20d6ca22024c1059036ad3f189e1ee
Juliamello8 <juliainline@gmail.com>
2021-03-30 21:44:52 -0300

    REST Aula implementada

--- app/Http/Controllers/AulaController.php
+++ app/Http/Controllers/AulaController.php
@@ -1,22 +1,70 @@
 <?php
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class AulaController extends Controller
 {
+    //GET
     public function show($id = null)
     {
-        echo 'Exibe um ou todos os recursos:';
+        if($id){
+            $aulas = \App\Models\Aula::find($id);
+        }else{
+            $aulas = \App\Models\Aula::all();
+        }
+        return response($aulas, 200, [
+            'Content-Type' => 'application/json'
+        ]); 
     }
-    public function create()
+    //POST
+    public function create(Request $request)
     {
-        echo 'Cria um recurso';
+        if(isset($request->nome)){
+            $aula = new \App\Models\Aula();
+            $aula->nome = $request->nome;
+            $aula->save();
+            return response($aula, 201, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'nome da aula não foi informado'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
-    public function update($id)
+    //PUT
+    public function update(Request $request, $id)
     {
-        echo 'Altera recurso existente ';
+        if(isset($request->nome) && $id) {
+            $aula = \App\Models\Aula::find($id);
+            $aula->nome = $request->nome;
+            $aula->save();
+            return response($aula, 200, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'nome da aula ou id não informado'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
+    //DELETE
     public function destroy($id)
     {
-        echo 'Deleta um recurso existente';
+        if($id) {
+            $aula = \App\Models\Aula::find($id);
+            $aula->delete();
+            return response($aula, 200, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'id não informado'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
 }
